39ce8056c26269aa8a6312fbb0cdb73ab8274c93
wimes <dev.wimes@gmail.com>
2023-03-24 21:13:31 +0900

    first commit

---
+++ ScratchPad-SwiftUI/ScratchPad-SwiftUI/ScratchPad_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchPad_SwiftUIApp.swift
+//  ScratchPad-SwiftUI
+//
+//  Created by Wimes on 2023/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ScratchPad_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
